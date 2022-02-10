0012e6506a8b5aacbaa9cf0ac12a524708f3f583
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-02-11 00:54:41 +0300

    Repository created.

---
+++ src/main/java/com/osc/spring/unit/test/practice/dataAccess/StudentDao.java
@@ -0,0 +1,19 @@
+package com.osc.spring.unit.test.practice.dataAccess;
+
+import com.osc.spring.unit.test.practice.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentDao extends JpaRepository<Student, Integer> {
+
+    @Query("" +
+            "SELECT CASE WHEN COUNT(s) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM Student s " +
+            "WHERE s.email = ?1"
+    )
+    Boolean selectExistsEmail(String email);
+
+}
