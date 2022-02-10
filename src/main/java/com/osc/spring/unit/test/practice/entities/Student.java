ef71ce58fd74624820b16ee304d2a3f48c75e891
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-02-11 00:41:01 +0300

    Entity created.

---
+++ src/main/java/com/osc/spring/unit/test/practice/entities/Student.java
@@ -0,0 +1,27 @@
+package com.osc.spring.unit.test.practice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+}
